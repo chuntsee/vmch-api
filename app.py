27c5156b1f5035e179873fb8a745e9c3affa7748
junxie <jxie0004@student.monash.edu>
2021-09-13 11:49:17 +0800

    api fix

--- app.py
+++ app.py
@@ -22,11 +22,59 @@ jsonclasses_integrate(app, cors={
     'encode_key': 'SECRET_VERY_SECRET'
 })
 
-app = Flask(__name__)
-
+@app.get('/statements')
+async def statements() -> Response:
+    return data(await Statement.find())
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
+@app.get('/statements/<id>')
+async def statement(id: str) -> Response:
+    return data(await Statement.id(id))
+
+
+@app.post('/statements')
+async def create_statement() -> Response:
+    return data(Statement(**request.json).save())
+
+
+@app.patch('/statements/<id>')
+async def update_statement(id: str) -> Response:
+    return data((await Statement.id(id)).set(**request.json).save())
+
+
+@app.delete('/statements/<id>')
+async def delete_statement(id: str) -> Response:
+    return empty((await Statement.id(id)).delete())
+
+@app.get('/statements/userid/<id>')
+async def user_statements(id: str) -> Response:
+    return data(await Statement.find(user_id=id))
+
+@app.get('/items')
+async def items() -> Response:
+    return data(await Item.find())
+
+
+@app.get('/items/<id>')
+async def item(id: str) -> Response:
+    return data(await Item.id(id))
+
+
+@app.post('/items')
+async def create_item() -> Response:
+    return data(Item(**request.json).save())
+
+
+@app.patch('/items/<id>')
+async def update_item(id: str) -> Response:
+    return data((await Item.id(id)).set(**request.json).save())
+
+
+@app.delete('/items/<id>')
+async def delete_item(id: str) -> Response:
+    return empty((await Item.id(id)).delete())
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0')
